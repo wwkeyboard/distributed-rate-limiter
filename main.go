05aa5c91f534e6a112470652a7652143c89ecb4d
Aaron Lee <alee@aaronosaur.us>
2020-01-27 10:57:03 -0600

    stub the server and rate limit middleware

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"io"
+	"log"
+	"net/http"
+)
+
+func rateLimit(f http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if true == true {
+			http.Error(w, http.StatusText(http.StatusTooManyRequests), http.StatusTooManyRequests)
+			return
+		}
+		f(w, r)
+	}
+}
+
+func test1(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Hello from test1!\n")
+}
 
 func main() {
-	fmt.Println("hello world")
+	http.HandleFunc("/test1", rateLimit(test1))
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
